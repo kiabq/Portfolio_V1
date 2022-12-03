3c748dd27c1b27de738896d0bf3f8e0ef13b2191
kiabq <conway.joseph@pm.me>
2022-12-03 23:55:59 +0000

    Update build file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kiabq.github.io",
-    base: "/Portfolio_V1"
+    site: "https://kiabq.github.io"
 });
