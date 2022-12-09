21ff306caf0c6db15dd79cf2595cde9836d0c13a
kiabq <conway.joseph@pm.me>
2022-12-09 03:28:45 -0500

    Move scripts to separate file

---
+++ src/scripts/preload.js
@@ -0,0 +1,20 @@
+const theme = localStorage.getItem("theme");
+const htmlTag = document.getElementsByTagName("html");
+
+const stylesheet = document.styleSheets[4];
+stylesheet.insertRule(".preload {}", stylesheet.cssRules.length);
+
+const preload = [...stylesheet.cssRules].find((val) => val.selectorText === ".preload");
+
+const light = stylesheet.cssRules[0].style.getPropertyValue("--lightTheme");
+const dark = stylesheet.cssRules[0].style.getPropertyValue("--darkTheme");
+
+if (theme === "light" || theme === null) {
+    preload.style.setProperty("transform", "translateY(0px)");
+    htmlTag[0].style.backgroundColor = light;
+    htmlTag[0].style.color = dark;
+} else {
+    preload.style.setProperty("transform", "translateY(-28px)");
+    htmlTag[0].style.backgroundColor = dark;
+    htmlTag[0].style.color = light;
+}
