4e12a242bfd232080bebaabde15f41eda4390f8f
kiabq <conway.joseph@pm.me>
2023-01-29 18:35:29 -0500

    Update game + add ability to place/delete cells

--- src/scripts/cgol.ts
+++ src/scripts/cgol.ts
@@ -1,103 +1,120 @@
-let canvas = document.getElementById("game") as HTMLCanvasElement;
+type Board = {
+    alive: number,
+    x: number,
+    y: number
+}
+
 
 function cgol() {
+    let canvas = document.getElementById("game") as HTMLCanvasElement;
+    let pauseButton = document.getElementById("pause") as HTMLButtonElement;
+
     if (canvas !== null) {
         let ctx = canvas.getContext("2d");
         let height = canvas.width;
         let width = canvas.height;
 
-        let board: Array<number>[] = [];
-        let x = 0;
-        let y = 0;
-
+        let board: Array<Board>[] = [];
+        let xOffset = 0, yOffset = 0;
         let border = 0;
 
-        let interval = null;
-        
-        let initial = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
+        let interval: number | undefined = undefined;
+        let paused = false;
 
-        function createBoard(height: number, width: number, borderWidth: number) {
-            for (let i = 0; i < height; i++) {
+        let initial = [
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+
+        let boardSize = initial.length;
+
+        let block = {
+            x: width / boardSize,
+            y: height / boardSize
+        }
+
+        function createBoard(size: number, borderWidth: number) {
+            for (let i = 0; i < size; i++) {
                 board.push([]);
 
-                for (let j = 0; j < width; j++) {
+                for (let j = 0; j < size; j++) {
                     if (initial[i][j] === 1) {
-                        board[i].push(1)
+                        board[i].push({ alive: 1, x: j * block.x, y: i * block.y })
                     } else {
-                        board[i].push(0)
+                        board[i].push({ alive: 0, x: j * block.x, y: i * block.y })
                     }
                 }
 
                 border = borderWidth;
             }
-
-            x = board[0].length;
-            y = board.length;
         }
 
         function init() {
-            createBoard(initial.length, initial[0].length, 2);
+            createBoard(boardSize, 2);
 
             draw();
 
-            interval = setInterval(newFrame, 200)
+            interval = setInterval(newFrame, 200);
         }
 
         function draw() {
-            for (let i = 0; i < y; i++) {
-                for (let j = 0; j < x; j++) {
-                    let xOffset = border;
-                    let yOffset = border;
+            for (let i = 0; i < boardSize; i++) {
+                for (let j = 0; j < boardSize; j++) {
+                    xOffset = border;
+                    yOffset = border;
 
-                    if (j === x - 1) {
+                    if (j === boardSize - 1) {
                         xOffset = 0;
                     }
 
-                    if (i === y - 1) {
+                    if (i === boardSize - 1) {
                         yOffset = 0;
                     }
 
-                    if (board[i][j] === 1) {
+                    ctx!.beginPath();
+                    ctx!.rect(
+                        Math.round(board[i][j].x),
+                        Math.round(board[i][j].y),
+                        Math.round(block.x - xOffset),
+                        Math.round(block.y - yOffset),
+                    );
+                    
+                    if (board[i][j].alive === 1) {
                         ctx!.fillStyle = "yellow";
                     } else {
                         ctx!.fillStyle = "grey";
                     }
 
-                    ctx?.fillRect(
-                        (width / x) * j, 
-                        (height / x) * i, 
-                        width / x - xOffset, 
-                        height / x - yOffset
-                    );
+                    ctx!.fill();
                 }
             }
         }
         
         function game() {
-            let newBoard: Array<number>[] = [];
+            let newBoard: Array<Board>[] = [];
 
-            for (let i = 0; i < y; i++) {
-                let buffer = [];
+            for (let i = 0; i < boardSize; i++) {
+                newBoard.push([]);
 
-                for (let j = 0; j < x; j++) {
+                for (let j = 0; j < boardSize; j++) {
                     let score = 0;
                     
                     let edgeCheck = [
@@ -116,32 +133,109 @@ function cgol() {
                         let eX = edgeCheck[k][1];
                         if (board[eY] !== undefined) {
                             if (board[eX] !== undefined) {
-                                score += board[eY][eX];
+                                score += board[eY][eX].alive;
                             }
                         }
                     }
 
-                    if (score === 3) {
-                        buffer.push(1);
-                    } else if (board[i][j] === 1 && score >= 2 && score <= 3) {
-                        buffer.push(1);
+                    if (score === 3 || (board[i][j].alive === 1 && score >= 2 && score <= 3)) {
+                        newBoard[i].push({ alive: 1, x: j * block.x, y: i * block.y });
                     } else {
-                        buffer.push(0);
+                        newBoard[i].push({ alive: 0, x: j * block.x, y: i * block.y });
                     }
                 }
-
-                newBoard.push(buffer);
             }
 
             board = newBoard;
         }
+
+        function clickEvent(this: HTMLCanvasElement, e: MouseEvent) {
+            let area = this.getBoundingClientRect();
+            
+            let posX = e.clientX - area.left;
+            let posY = e.clientY - area.top;
+
+            let x = Math.floor(posX / block.x);
+            let y = Math.floor(posY / block.y)
+
+            if (board[y][x].alive === 0) {
+                board[y][x].alive = 1;
+            } else {
+                board[y][x].alive = 0;
+            }
+            
+            draw();
+        };
+
+        function mouseOverEvent(this: HTMLCanvasElement, e: MouseEvent) {
+            let area = this.getBoundingClientRect();
+
+            let posX = e.clientX - area.left;
+            let posY = e.clientY - area.top;
+
+            for (let i = 0; i < boardSize; i++) {
+                for (let j = 0; j < boardSize; j++) {
+                    xOffset = border;
+                    yOffset = border;
+
+                    if (j === boardSize - 1) {
+                        xOffset = 0;
+                    }
+    
+                    if (i === boardSize - 1) {
+                        yOffset = 0;
+                    }
+    
+                    ctx!.beginPath();
+                    ctx!.rect(
+                        Math.round(board[i][j].x), 
+                        Math.round(board[i][j].y), 
+                        Math.round(block.x - xOffset), 
+                        Math.round(block.y - yOffset)
+                    );
+                    
+                    if (ctx!.isPointInPath(posX, posY)) {
+                        ctx!.fillStyle = "blue";
+                    } else if (board[i][j].alive) {
+                        ctx!.fillStyle = "yellow";
+                    } else {
+                        ctx!.fillStyle = "grey";
+                    }
+                    
+                    ctx!.fill();
+                }
+            }
+        }
+
+        clickEvent.bind(canvas);
+        mouseOverEvent.bind(canvas);
+
+        pauseButton.addEventListener("click", () => {
+            if (paused) {
+                canvas.removeEventListener("click", clickEvent);
+                canvas.removeEventListener("mousemove", mouseOverEvent);
+
+                pauseButton.textContent = "Pause";
+
+                paused = false;
+                interval = setInterval(newFrame, 200);
+            } else {
+                paused = true;
+                clearInterval(interval);
+
+                pauseButton.textContent = "Resume";
+
+                canvas.addEventListener("click", clickEvent);
+                canvas.addEventListener("mousemove", mouseOverEvent);
+            }
+        })
 
         function newFrame() {
             game();
             draw();        
         }
 
-        if (interval === null) {
+        if (interval === undefined) {
             init();
         }
     }
