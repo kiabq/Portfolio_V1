77f26eca40f6d386129fbbb56260e8fc826f5fab
kiabq <conway.joseph@pm.me>
2026-06-20 01:16:36 -0400

    add: theme and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  fonts: [{
+      provider: fontProviders.local(),
+      name: "Sentient",
+      cssVariable: "--font-sentient",
+      options: {
+          variants: [
+              {
+                  src: ['./src/assets/fonts/Sentient-Variable.woff2'],
+                  weight: "100 900",
+                  style: 'normal'
+              },
+              {
+                  src: ['./src/assets/fonts/Sentient-VariableItalic.woff2'],
+                  weight: "100 900",
+                  style: 'italic'
+              }
+          ],
+
+      }
+  }],
+  integrations: [icon()]
+});
