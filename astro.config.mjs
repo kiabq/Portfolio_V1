f7f77ddf4e4df839337d35d0018321bc61c20964
kiabq <conway.joseph@pm.me>
2026-06-21 21:53:19 -0400

    add: transitions via Intersect Observer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,34 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
   },
 
-  fonts: [{
+  fonts: [
+    {
       provider: fontProviders.local(),
       name: "Sentient",
       cssVariable: "--font-sentient",
       options: {
-          variants: [
-              {
-                  src: ['./src/assets/fonts/Sentient-Variable.woff2'],
-                  weight: "100 900",
-                  style: 'normal'
-              },
-              {
-                  src: ['./src/assets/fonts/Sentient-VariableItalic.woff2'],
-                  weight: "100 900",
-                  style: 'italic'
-              }
-          ],
-
-      }
-  }],
-  integrations: [icon()]
+        variants: [
+          {
+            src: ["./src/assets/fonts/Sentient-Variable.woff2"],
+            weight: "100 900",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/Sentient-VariableItalic.woff2"],
+            weight: "100 900",
+            style: "italic",
+          },
+        ],
+      },
+    },
+  ],
+  integrations: [icon()],
 });
---
+++ src/lib.ts
@@ -0,0 +1,11 @@
+export const TRANSITION_DURATION = 700;
+export const TRANSITION_IN = ["translate-y-0", "opacity-100"];
+export const TRANSITION_OUT = ["translate-y-8", "opacity-0"];
+export const TRANSITION = [
+  "opacity-0",
+  "translate-y-8",
+  "transition-translate",
+  "ease-out",
+  "motion-reduce:transition-none",
+  "motion-reduce:translate-none",
+];
